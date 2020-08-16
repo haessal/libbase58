bcdd1d55b311a4e5acc5cf63000218a2902ad155
haessal <haessal@mizutamauki.net>
2020-08-16 21:53:34 +0900

    Display P2SH address to check if it starts with '3'

--- base58.c
+++ base58.c
@@ -1,144 +1,35 @@
-/*
- * Copyright 2012-2014 Luke Dashjr
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the standard MIT license.  See COPYING for more details.
- */
-
-#ifndef WIN32
-#include <arpa/inet.h>
-#else
-#include <winsock2.h>
-#endif
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
+bool b58enc(char *b58, size_t *b58sz, const void *data, size_t binsz);
 
-#include "libbase58.h"
-
-bool (*b58_sha256_impl)(void *, const void *, size_t) = NULL;
-
-static const int8_t b58digits_map[] = {
-	-1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-	-1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-	-1,-1,-1,-1,-1,-1,-1,-1, -1,-1,-1,-1,-1,-1,-1,-1,
-	-1, 0, 1, 2, 3, 4, 5, 6,  7, 8,-1,-1,-1,-1,-1,-1,
-	-1, 9,10,11,12,13,14,15, 16,-1,17,18,19,20,21,-1,
-	22,23,24,25,26,27,28,29, 30,31,32,-1,-1,-1,-1,-1,
-	-1,33,34,35,36,37,38,39, 40,41,42,43,-1,44,45,46,
-	47,48,49,50,51,52,53,54, 55,56,57,-1,-1,-1,-1,-1,
-};
-
-bool b58tobin(void *bin, size_t *binszp, const char *b58, size_t b58sz)
+int main(int argc, char** argv)
 {
-	size_t binsz = *binszp;
-	const unsigned char *b58u = (void*)b58;
-	unsigned char *binu = bin;
-	size_t outisz = (binsz + 3) / 4;
-	uint32_t outi[outisz];
-	uint64_t t;
-	uint32_t c;
-	size_t i, j;
-	uint8_t bytesleft = binsz % 4;
-	uint32_t zeromask = bytesleft ? (0xffffffff << (bytesleft * 8)) : 0;
-	unsigned zerocount = 0;
-	
-	if (!b58sz)
-		b58sz = strlen(b58);
-	
-	memset(outi, 0, outisz * sizeof(*outi));
-	
-	// Leading zeros, just count
-	for (i = 0; i < b58sz && b58u[i] == '1'; ++i)
-		++zerocount;
-	
-	for ( ; i < b58sz; ++i)
-	{
-		if (b58u[i] & 0x80)
-			// High-bit set on invalid digit
-			return false;
-		if (b58digits_map[b58u[i]] == -1)
-			// Invalid base58 digit
-			return false;
-		c = (unsigned)b58digits_map[b58u[i]];
-		for (j = outisz; j--; )
-		{
-			t = ((uint64_t)outi[j]) * 58 + c;
-			c = (t & 0x3f00000000) >> 32;
-			outi[j] = t & 0xffffffff;
-		}
-		if (c)
-			// Output number too big (carry to the next int32)
-			return false;
-		if (outi[0] & zeromask)
-			// Output number too big (last int32 filled too far)
-			return false;
-	}
-	
-	j = 0;
-	switch (bytesleft) {
-		case 3:
-			*(binu++) = (outi[0] &   0xff0000) >> 16;
-		case 2:
-			*(binu++) = (outi[0] &     0xff00) >>  8;
-		case 1:
-			*(binu++) = (outi[0] &       0xff);
-			++j;
-		default:
-			break;
-	}
-	
-	for (; j < outisz; ++j)
-	{
-		*(binu++) = (outi[j] >> 0x18) & 0xff;
-		*(binu++) = (outi[j] >> 0x10) & 0xff;
-		*(binu++) = (outi[j] >>    8) & 0xff;
-		*(binu++) = (outi[j] >>    0) & 0xff;
-	}
-	
-	// Count canonical base58 byte count
-	binu = bin;
-	for (i = 0; i < binsz; ++i)
-	{
-		if (binu[i])
-			break;
-		--*binszp;
-	}
-	*binszp += zerocount;
-	
-	return true;
+	char in[25] = {0x05,};
+	char out[35];
+	size_t outSize = sizeof(out);
+	size_t inSize = sizeof(in);
+	int i;
+	bool result;
+
+//	05 00000000 00000000 00000000 00000000 00000000 00000000h = 31h1vYVSYuKP6AhS86fbRdMw9XHiZAvAaj
+//	05 00000000 00000000 00000000 00000000 00000000 00000001h = 31h1vYVSYuKP6AhS86fbRdMw9XHiZAvAak
+//	05 00000000 00000000 00000000 00000000 00000000 00000002h = 31h1vYVSYuKP6AhS86fbRdMw9XHiZAvAam
+//	...
+//	05 ffffffff ffffffff ffffffff ffffffff ffffffff fffffffdh = 3R2cuenjG5nFubqX9Wzuukdin2YfGQVzu2
+//	05 ffffffff ffffffff ffffffff ffffffff ffffffff fffffffeh = 3R2cuenjG5nFubqX9Wzuukdin2YfGQVzu3
+//	05 ffffffff ffffffff ffffffff ffffffff ffffffff ffffffffh = 3R2cuenjG5nFubqX9Wzuukdin2YfGQVzu4
+
+	result = b58enc(out, &outSize, (void *)in, inSize);
+	if (result)
+		printf("%s", out);
+	else
+		printf("error(%zu)", outSize);
 }
 
-static
-bool my_dblsha256(void *hash, const void *data, size_t datasz)
-{
-	uint8_t buf[0x20];
-	return b58_sha256_impl(buf, data, datasz) && b58_sha256_impl(hash, buf, sizeof(buf));
-}
-
-int b58check(const void *bin, size_t binsz, const char *base58str, size_t b58sz)
-{
-	unsigned char buf[32];
-	const uint8_t *binc = bin;
-	unsigned i;
-	if (binsz < 4)
-		return -4;
-	if (!my_dblsha256(buf, bin, binsz - 4))
-		return -2;
-	if (memcmp(&binc[binsz - 4], buf, 4))
-		return -1;
-	
-	// Check number of zeros is correct AFTER verifying checksum (to avoid possibility of accessing base58str beyond the end)
-	for (i = 0; binc[i] == '\0' && base58str[i] == '1'; ++i)
-	{}  // Just finding the end of zeros, nothing to do in loop
-	if (binc[i] == '\0' || base58str[i] == '1')
-		return -3;
-	
-	return binc[0];
-}
 
 static const char b58digits_ordered[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
 
@@ -182,20 +73,4 @@ bool b58enc(char *b58, size_t *b58sz, const void *data, size_t binsz)
 	*b58sz = i + 1;
 	
 	return true;
-}
-
-bool b58check_enc(char *b58c, size_t *b58c_sz, uint8_t ver, const void *data, size_t datasz)
-{
-	uint8_t buf[1 + datasz + 0x20];
-	uint8_t *hash = &buf[1 + datasz];
-	
-	buf[0] = ver;
-	memcpy(&buf[1], data, datasz);
-	if (!my_dblsha256(hash, buf, datasz + 1))
-	{
-		*b58c_sz = 0;
-		return false;
-	}
-	
-	return b58enc(b58c, b58c_sz, buf, 1 + datasz + 4);
 }
